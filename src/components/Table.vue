66e206153cd5530e3bb035b0520f2fe4ce276ce9
tikesh <tikeshisverma@gmail.com>
2021-05-15 01:35:16 +0530

    [FEAT]:adding next and previous button and select

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="table">
-    <Row  v-for="item in data.data.coins" :key="item.id" :rowData="item"/>
+    <Row
+      v-for="item in data.data.coins.slice(start, end)"
+      :key="item.id"
+      :rowData="item"
+    />
+    <div class="range">{{start}} - {{end}}</div>
+    <div class="button-wrepper">
+      <button v-if="start > 0" class="previous" @click="previous10">
+        PREVIOUS
+      </button>
+      <button v-if="end < 100" class="next" @click="next10">NEXT</button>
+    </div>
 
+    <select v-model.number="rowPerPage" name="row-count" @change="updateRowsPerPage">
+      <option>10</option>
+      <option>25</option>
+      <option>50</option>
+      <option>100</option>
+    </select>
   </div>
 </template>
 
@@ -13,7 +30,29 @@ export default {
   components: {
     Row,
   },
-  props: {data:Object},
+  data() {
+    return {
+      start: 0,
+      end: 10,
+      rowPerPage:10
+    };
+  },
+
+  props: { data: Object },
+  methods: {
+    next10() {
+      this.start += this.rowPerPage;
+      this.end +=this.rowPerPage;
+    },
+    previous10() {
+      this.start -= this.rowPerPage;
+      this.end -= this.rowPerPage;
+    },
+    updateRowsPerPage(){
+      this.start = 0;
+      this.end = this.start + this.rowPerPage
+    }
+  },
 };
 </script>
 
